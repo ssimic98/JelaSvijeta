5d166e15e48ea566e7fd2bcac665f55bad818a43
ssimic98 <ssimic@etfos.hr>
2024-06-13 14:46:52 +0200

    failedValidation and messages added

--- app/Http/Requests/MealRequest.php
+++ app/Http/Requests/MealRequest.php
@@ -3,7 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 class MealRequest extends FormRequest
 {
     /**
@@ -25,9 +26,23 @@ class MealRequest extends FormRequest
             'category'=>'sometimes|nullable|string',
             'lang'=>'required|string|exists:languages,code',
             'per_page'=>'sometimes|int|min:1',
-            'diff_time'=>'sometimes|int|min:0',
+            'diff_time'=>'sometimes|int|min:1',
             'tags'=>'sometimes|string',
-            'ingredients'=>'sometimes|string'
+            'ingredients'=>'sometimes|string',
+            'with'=>'sometimes|string',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(),422));
+    }
+
+    public function messages()
+    {
+        return 
+        [
+            'lang.exists'=>'Lang can only be hr or en',
         ];
     }
 }
