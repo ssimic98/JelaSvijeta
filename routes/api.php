31d8feba35a9243390c35e00d2e7827843936e27
ssimic98 <ssimic@etfos.hr>
2024-06-13 14:49:28 +0200

    Api route added

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,4 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MealController;
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::get('meals',[MealController::class,'index']);
+Route::get('/meals',[MealController::class,'index']);
